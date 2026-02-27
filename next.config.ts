2091f1ed4bcf11cbad747a255aa4e5627871c825
JesusD2405 <jesusdavid2405@gmail.com>
2026-02-27 14:06:53 -0400

    fix: mejorar compatibilidad con GitHub Pages (trailingSlash y .nojekyll)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: "/projects-portfolio",
+  trailingSlash: true, // Mejora la compatibilidad con GitHub Pages
   images: {
     unoptimized: true,
   },
